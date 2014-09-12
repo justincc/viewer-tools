0e6f6e28822a8ace5f1273c2d3a7b094b902b0ca
Justin Clark-Casey <justincc@justincc.org>
2014-09-12 22:02:54 +0100

    add output path and date options to eml.py

--- extract/extract-meeting-log/src/eml.py
+++ extract/extract-meeting-log/src/eml.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python
 
 import argparse
+import datetime
+import string
 
 ############
 ### MAIN ###
@@ -8,14 +10,32 @@ import argparse
 parser = argparse.ArgumentParser(formatter_class = argparse.RawTextHelpFormatter)
 
 parser.add_argument(
+    '--date',
+    help = "Date in the format <y>-<m>-<d> for which to produce log.  For example, 2014-09-09.  If omitted then is today's date.",
+    default = datetime.date.today().strftime("%Y-%m-%d"),
+    metavar = "<date>")
+
+parser.add_argument(
+    'outputPath',
+    help = "Output path for the generated chat log.",
+    metavar = "<output-path>")
+
+parser.add_argument(
     'logPath', 
     help = "Path to the chat log file.", 
-    metavar = "chat-log-path",
-    nargs='*')
+    metavar = "<chat-log-path>",
+    nargs = '+')
 
 opt = parser.parse_args()
 
-for path in opt.logPath:
-    with open(path) as f:
-        for line in f:
-            print line
+#print "Date: %s" % opt.date
+#print "LogPath: %s" % opt.logPath
+
+date = opt.date.translate(string.maketrans("-", "/"))
+
+with open(opt.outputPath, 'w') as o:
+    for path in opt.logPath:
+        with open(path) as f:
+            for line in f:
+                if line.startswith("[%s" % date):
+                    o.write(line)
